4a85f84fbc5a5bb8a5ddd9bb67cfba0df5eb6406
gwlodawiec <gwlodawiec@gmail.com>
2021-01-21 14:24:27 +0100

    First approach and tests.

---
+++ src/main/java/com/example/demo/service/OccurenciesCounter.java
@@ -0,0 +1,22 @@
+package com.example.demo.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OccurenciesCounter {
+	
+	public Map<String, Integer> count(String[] input) {
+		Map<String, Integer> result = new HashMap<>();
+		
+		if(input == null || input.length == 0) {
+			return result;
+		}
+		
+		for(int i = 0; i < input.length; i++) {
+			result.merge(input[i], 1, Integer::sum);
+		}
+		
+		return result;
+	}
+
+}
---
+++ src/test/java/com/example/demo/service/OccurenciesCounterTest.java
@@ -0,0 +1,38 @@
+package com.example.demo.service;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.hamcrest.collection.IsMapContaining;
+import org.junit.jupiter.api.Test;
+
+public class OccurenciesCounterTest {
+	
+	@Test
+	public void occurenciesShouldBeCountedProperly() {
+		
+		OccurenciesCounter counter = new OccurenciesCounter();
+		String[] stringArray = {"a","b","b","b","b","b","d","h","h","h","p"};
+		Map<String, Integer> result = counter.count(stringArray);
+		
+		Map<String, Integer> expected = new HashMap<>();
+		expected.put("a", 1);
+		expected.put("b", 5);
+		expected.put("d", 1);
+		expected.put("h", 3);
+		expected.put("p", 1);
+		
+		assertThat(result.isEmpty(), is(false));
+		
+		assertThat(result, IsMapContaining.hasEntry("a", 1));
+		assertThat(result, IsMapContaining.hasEntry("b", 5));
+		assertThat(result, IsMapContaining.hasEntry("d", 1));
+		assertThat(result, IsMapContaining.hasEntry("h", 3));
+		assertThat(result, IsMapContaining.hasEntry("p", 1));
+		
+	}
+
+}
